87994e3ac752faa3ad727fb074bc9386a2c71fd2
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2024-10-29 12:24:47 +0100

    Structure, environment and packages

---
+++ immo_eliza/preprocessing.py
@@ -0,0 +1,12 @@
+def import_df():
+    '''
+    This will take the csv from the data folder called raw_data.csv, turn it into a df and return
+    '''
+
+def preprocess(df):
+    '''
+    Takes a df, will go through all needed preprocessing steps and return the cleaned df.
+    '''
+
+
+    return df
